0c6c93314a7ebe280402f6f1d890b4b39cd1ee31
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2020-05-02 20:58:16 +0200

    init commit #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // <-- this
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/my-component/my-component.component.ts
@@ -0,0 +1,39 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.css']
+})
+export class MyComponentComponent  {
+  private _greetings: string[];
+  public greeting: string;
+  private _index: number;
+
+  public foo() {return "apple";} 
+  constructor() {
+      this._greetings = [
+          'Jó reggelt!',
+          'Good morning!',
+          'Guten tag!',
+          'Buenos dias!'
+      ];
+      this._index = 0;
+
+      this._tick();
+  }
+
+  private _tick(): void {
+      this.greeting = this._greetings[this._index];
+
+      if (this._index + 1 < this._greetings.length) {
+          ++this._index;
+      }
+      else {
+          this._index = 0
+      }
+
+      setTimeout(this._tick.bind(this), 1000)
+  }
+
+}

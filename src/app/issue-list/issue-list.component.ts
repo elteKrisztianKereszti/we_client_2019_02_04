2f1d949d8405f707b30b66c97c1561a7c617de11
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2020-05-08 22:02:08 +0200

    store issues in variable and render

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Issue } from '../issue';
 
 @Component({
   selector: 'app-issue-list',
@@ -6,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./issue-list.component.css']
 })
 export class IssueListComponent implements OnInit {
+
+  public issues: Issue[] = [
+    {
+      id: 1,
+      place: 'PC42',
+      title: 'The issue #1',
+      description: 'Fatallica',
+      status: 'NEW',
+    },
+    {
+      id: 2,
+      place: 'PC12',
+      title: 'MegaIssue',
+      description: 'MegaFatal',
+      status: 'DOING',
+    },
+    {
+      id: 3,
+      place: 'Air',
+      title: 'Unknown issue',
+      description: 'U.F.O',
+      status: 'DOING',
+    },
+    {
+      id: 4,
+      place: 'PCxxx',
+      title: 'Broken Windows 10',
+      description: 'Everywhere',
+      status: 'DONE'
+    },
+  ];
 
   constructor() { }
 
---
+++ src/app/issue.ts
@@ -0,0 +1,7 @@
+export class Issue {
+  public id: number = 0;
+  public place: string = '';
+  public title: string = '';
+  public description: string = '';
+  public status: string = '';
+}

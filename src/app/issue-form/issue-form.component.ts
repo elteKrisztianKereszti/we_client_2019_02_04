d6440bb7a28317034b2ae79bba4cf4bafaa8cef7
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2020-05-09 07:19:44 +0200

    Use reactive forms to propagate issue data on UI (issueform)

--- src/app/issue-form/issue-form.component.ts
+++ src/app/issue-form/issue-form.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Issue } from '../issue';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-issue-form',
   templateUrl: './issue-form.component.html',
   styleUrls: ['./issue-form.component.css']
 })
-export class IssueFormComponent implements OnInit {
+export class IssueFormComponent implements OnChanges {
   @Input() issue: Issue
-  
-  constructor() { }
 
-  public ngOnInit(): void {
+  constructor(private fb: FormBuilder) { }
+
+  public form: FormGroup = this.fb.group({
+    title: ['', [Validators.required]],
+    place: ['', [Validators.required, Validators.pattern(/^PC\d+/)]],
+    description: [''],
+    status: ['NEW', [Validators.required]]
+  });
+
+  get title() { return this.form.get('title'); }
+  get place() { return this.form.get('place'); }
+  get description() { return this.form.get('description'); }
+  get status() { return this.form.get('status'); }
+
+  public ngOnChanges(): void {
+    this.form.patchValue(this.issue);
   }
-
 }
